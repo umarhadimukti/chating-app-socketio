c681060fa88552c2d474ea3be62c9dd49928a96e
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-05-02 16:14:17 +0700

    fix first setup server

--- server.ts
+++ server.ts
@@ -1,10 +1,16 @@
 import express, { Express } from 'express';
+import http from 'http';
+import path from 'path';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app: Express = express();
+const server: http.Server = http.createServer(app);
 
-const PORT = process.env.PORT || 3000;
+const PORT: number = Number(process.env.PORT) || 3000;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+// built-in middleware
+app.use(express.static(path.join(__dirname, 'public')));
+
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
