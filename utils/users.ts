ef736da6e2942ae2275ca198eee4ae5b0fb66701
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-05-04 18:18:48 +0700

    add comment

--- utils/users.ts
+++ utils/users.ts
@@ -6,16 +6,19 @@ interface IUserJoin {
 
 let users: IUserJoin[] = [];
 
+// handle when a user joins (add to array)
 export const userJoin = (socketId: string, username: string, room: string) => {
     const user = { socketId, username, room };
     users.push(user);
     return user;
 }
 
+// get current user
 export const getCurrentUser = (socketId: string) => {
     return users.find(user => user.socketId === socketId);
 }
 
+// runs when client disconnects
 export const userLeave = (socketId: string) => {
     const index = users.findIndex(user => user.socketId === socketId);
     if (index !== -1) {
@@ -23,6 +26,7 @@ export const userLeave = (socketId: string) => {
     }
 }
 
+// get room users
 export const getRoomUsers = (room: string) => {
     return users.filter(user => user.room === room);
 }
